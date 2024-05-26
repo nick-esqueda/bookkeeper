be65e752eab9c251f24c9bea607532ae814e355c
nick-esqueda <nick_esqueda@outlook.com>
2024-05-25 19:08:13 -0500

    data model: added flyway migration to seed books. modified seed migration for book categories.

--- backend/src/main/resources/db/migration/V3__seed_book_categories.sql
+++ backend/src/main/resources/db/migration/V3__seed_book_categories.sql
@@ -1,6 +1,6 @@
-INSERT INTO book_categories (id, name, created_at, updated_at)
+INSERT INTO book_categories (name, created_at, updated_at)
 VALUES
-(1, 'fiction', NOW(), NOW()),
-(2, 'non-fiction', NOW(), NOW()),
-(3, 'classics', NOW(), NOW()),
-(4, 'cookbooks', NOW(), NOW());
+('fiction', NOW(), NOW()),
+('non-fiction', NOW(), NOW()),
+('classics', NOW(), NOW()),
+('cookbooks', NOW(), NOW());
---
+++ backend/src/main/resources/db/migration/V4__seed_books.sql
@@ -0,0 +1,3 @@
+INSERT INTO books (book_category_id, title, author, edition, notes, read_status, created_at, updated_at)
+VALUES
+(1, 'A Fragile Enchantment', 'Allison Saft', NULL, NULL, 'UNREAD', NOW(), NOW());
