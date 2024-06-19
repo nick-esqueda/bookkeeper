c19c49d5d8038ab0a49a3301b6068802380deae1
nick-esqueda <nick_esqueda@outlook.com>
2024-06-19 13:58:06 -0500

    styles: added fade-in on category card component.

--- frontend/src/components/CategoryCard.js
+++ frontend/src/components/CategoryCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Badge, Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Fade, Badge, Card } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
@@ -7,22 +7,32 @@ const CategoryCard = ({ categoryId }) => {
   const category = useSelector(
     (state) => state.bookCategories.entities[categoryId]
   );
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   return (
-    <Link to={`/categories/${category.id}`} style={{ textDecoration: "none" }}>
-      <Card className="shadow-lg">
-        <Card.Header className="text-center">Category</Card.Header>
-        <Card.Body className="text-center m-5">
-          <Card.Title as={"h3"}>
-            {category.name}
-          </Card.Title>
-          <Card.Text className="d-flex justify-content-center gap-2 mt-3">
-              <Badge bg="success">{category.readBookCount} Read</Badge>
-              <Badge bg="primary">{category.totalBookCount} Total</Badge>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </Link>
+    category && (
+      <Fade in={isMounted}>
+        <Link
+          to={`/categories/${category.id}`}
+          style={{ textDecoration: "none" }}
+        >
+          <Card className="shadow-lg">
+            <Card.Header className="text-center">Category</Card.Header>
+            <Card.Body className="text-center m-5">
+              <Card.Title as={"h3"}>{category.name}</Card.Title>
+              <Card.Text className="d-flex justify-content-center gap-2 mt-3">
+                <Badge bg="success">{category.readBookCount} Read</Badge>
+                <Badge bg="primary">{category.totalBookCount} Total</Badge>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Link>
+      </Fade>
+    )
   );
 };
 
