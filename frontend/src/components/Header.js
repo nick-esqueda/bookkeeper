11cff95a11134777d91292f099d5d72bb7a3c016
nick-esqueda <nick_esqueda@outlook.com>
2024-06-20 20:29:05 -0500

    bugfix: navbar collapse menu was not closing after clicking a menu item.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -11,7 +11,12 @@ const Header = () => {
   const [modalShow, setModalShow] = useState(false);
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+    <Navbar
+      expand="lg"
+      className="bg-body-tertiary"
+      fixed="top"
+      collapseOnSelect
+    >
       <Container>
         <Navbar.Brand as={Link} to="/" id="nav-brand">
           Lacey Bee's Book Inventory
@@ -19,10 +24,10 @@ const Header = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="text-end">
-            <Nav.Link as={Link} to="/categories">
+            <Nav.Link as={Link} to="/categories" eventKey="1">
               Categories
             </Nav.Link>
-            <Nav.Link as={Link} to="/books">
+            <Nav.Link as={Link} to="/books" eventKey="2">
               Books
             </Nav.Link>
           </Nav>
