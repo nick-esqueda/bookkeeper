68e20d4e9916fa2be65d24d8136a274f05828de6
nick-esqueda <nick_esqueda@outlook.com>
2024-06-20 20:58:39 -0500

    bugfix: styles: navbar collapse menu not closing after clicking Add Book. refactored to utilize useState to track and set navExpanded state. bugfix: styles: Add Book button was left aligned with other nav items on desktop viewports.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,30 +9,48 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   const [modalShow, setModalShow] = useState(false);
+  const [navExpanded, setNavExpanded] = useState(false);
+
+  const handleAddBookClick = () => {
+    setModalShow(true);
+    setNavExpanded(false);
+  };
 
   return (
     <Navbar
       expand="lg"
       className="bg-body-tertiary"
       fixed="top"
-      collapseOnSelect
+      expanded={navExpanded}
     >
       <Container>
         <Navbar.Brand as={Link} to="/" id="nav-brand">
           Lacey Bee's Book Inventory
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={() => setNavExpanded(!navExpanded)}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="text-end">
-            <Nav.Link as={Link} to="/categories" eventKey="1">
+          {/* set Add Book button at end of navbar on non-mobile viewports. */}
+          <Nav className={navExpanded ? "text-end" : "me-auto"}>
+            <Nav.Link
+              as={Link}
+              to="/categories"
+              onClick={() => setNavExpanded(!navExpanded)}
+            >
               Categories
             </Nav.Link>
-            <Nav.Link as={Link} to="/books" eventKey="2">
+            <Nav.Link
+              as={Link}
+              to="/books"
+              onClick={() => setNavExpanded(!navExpanded)}
+            >
               Books
             </Nav.Link>
           </Nav>
           <div className="text-end">
-            <Button variant="success" onClick={() => setModalShow(true)}>
+            <Button variant="success" onClick={handleAddBookClick}>
               Add Book
             </Button>
             <AddBookModal show={modalShow} onHide={() => setModalShow(false)} />
