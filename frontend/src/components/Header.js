b43fab52dde53f788f6264595c9d165f550fee71
nick-esqueda <nick_esqueda@outlook.com>
2024-06-20 22:05:55 -0500

    this commit reverts the previous two commits. 1. styles: set mobile navbar collapse menu to close when clicking anywhere outside the nav menu. 2. bugfix: styles: navbar collapse menu not closing after clicking Add Book. refactored to utilize useState to track and set navExpanded state. bugfix: styles: Add Book button was left aligned with other nav items on desktop viewports.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
@@ -8,64 +8,31 @@ import AddBookModal from "./AddBookModal";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
-  const navRef = useRef(null);
   const [modalShow, setModalShow] = useState(false);
-  const [navExpanded, setNavExpanded] = useState(false);
-
-  const handleAddBookClick = () => {
-    setModalShow(true);
-    setNavExpanded(false);
-  };
-
-  const handleClickOutsideNav = (e) => {
-    if (navRef.current && !navRef.current.contains(e.target)) {
-      setNavExpanded(false);
-    }
-  };
-
-  useEffect(() => {
-    // use event listeners and useRef to close navbar collapse menu when
-    // clicking anywhere outside the nav menu on mobile viewports.
-    document.addEventListener("click", handleClickOutsideNav, true);
-    return () =>
-      document.removeEventListener("click", handleClickOutsideNav, true);
-  }, []);
 
   return (
     <Navbar
       expand="lg"
       className="bg-body-tertiary"
       fixed="top"
-      expanded={navExpanded}
+      collapseOnSelect
     >
       <Container>
         <Navbar.Brand as={Link} to="/" id="nav-brand">
           Lacey Bee's Book Inventory
         </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          onClick={() => setNavExpanded(!navExpanded)}
-        />
-        <Navbar.Collapse id="basic-navbar-nav" ref={navRef}>
-          {/* set Add Book button at end of navbar on non-mobile viewports. */}
-          <Nav className={navExpanded ? "text-end" : "me-auto"}>
-            <Nav.Link
-              as={Link}
-              to="/categories"
-              onClick={() => setNavExpanded(!navExpanded)}
-            >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="text-end">
+            <Nav.Link as={Link} to="/categories" eventKey="1">
               Categories
             </Nav.Link>
-            <Nav.Link
-              as={Link}
-              to="/books"
-              onClick={() => setNavExpanded(!navExpanded)}
-            >
+            <Nav.Link as={Link} to="/books" eventKey="2">
               Books
             </Nav.Link>
           </Nav>
           <div className="text-end">
-            <Button variant="success" onClick={handleAddBookClick}>
+            <Button variant="success" onClick={() => setModalShow(true)}>
               Add Book
             </Button>
             <AddBookModal show={modalShow} onHide={() => setModalShow(false)} />
