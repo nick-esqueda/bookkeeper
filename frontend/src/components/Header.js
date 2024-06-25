1f85d6838d78080ae6fb7a75a5b5edd61ebae1f5
nick-esqueda <nick_esqueda@outlook.com>
2024-06-24 19:57:04 -0500

    bugfix: styles: navbar collapse menu not closing after clicking Add Book. refactored to utilize useState to track and set navExpanded state.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,29 +9,37 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   const [modalShow, setModalShow] = useState(false);
+  const [navExpanded, setNavExpanded] = useState(false);
+
+  const closeNav = () => {
+    setNavExpanded(false);
+  };
 
   return (
     <Navbar
       expand="lg"
+      expanded={navExpanded}
       className="bg-body-tertiary"
       fixed="top"
-      collapseOnSelect
     >
       <Container className="justify-content-start gap-1">
-        <Navbar.Toggle className="border-0" />
-        <Navbar.Brand as={Link} to="/" id="nav-brand">
+        <Navbar.Toggle
+          className="border-0"
+          onClick={() => setNavExpanded(!navExpanded)}
+        />
+        <Navbar.Brand as={Link} to="/" id="nav-brand" onClick={closeNav}>
           Lacey Bee's Book Inventory
         </Navbar.Brand>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/categories" eventKey="1">
+            <Nav.Link as={Link} to="/categories" onClick={closeNav}>
               Categories
             </Nav.Link>
-            <Nav.Link as={Link} to="/books" eventKey="2">
+            <Nav.Link as={Link} to="/books" onClick={closeNav}>
               Books
             </Nav.Link>
           </Nav>
-          <div>
+          <div onClick={closeNav}>
             <Button variant="success" onClick={() => setModalShow(true)}>
               Add Book
             </Button>
