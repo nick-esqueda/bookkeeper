2b534c53d5d6a684f705d1b1a43f5e689345fab6
nick-esqueda <nick_esqueda@outlook.com>
2024-11-03 16:49:57 -0600

    devops: modified backend PROD restart script

--- scripts/restart-app.sh
+++ scripts/restart-app.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+HOME_DIR="/home/ec2-user"
+
 # Run the DB backup script
-~/backup-mysql-to-s3.sh
+$HOME_DIR/backup-mysql-to-s3.sh
 
 # Shut the app down
-docker-compose -f compose-prod.yaml --env-file .env.production down
+docker-compose -f $HOME_DIR/compose-prod.yaml --env-file $HOME_DIR/.env.production down
 
 # Remove all images so that Docker pulls the latest ECR image on startup
 docker rmi $(docker images -aq)
 
 # Start the app
-docker-compose -f compose-prod.yaml --env-file .env.production up -d
+docker-compose -f $HOME_DIR/compose-prod.yaml --env-file $HOME_DIR/.env.production up -d
 
